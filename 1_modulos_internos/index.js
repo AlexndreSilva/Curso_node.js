82ba0357fdc271f1ae17337e8c3364520ae6020b
AlexndreSilva <98479499+AlexndreSilva@users.noreply.github.com>
2022-08-31 17:59:42 -0300

    fists commit

---
+++ 1_modulos_internos/index.js
@@ -0,0 +1,9 @@
+//chamando o modulo
+const meuModulo = require('./meus_modulos/modulos')
+
+// criando const com o modulo pra simplificar a sintaxe
+const soma = meuModulo.soma
+
+// chamando o modulo inportado
+
+soma(9,20)
