ee2e6a2889a1cdf20b0a3bf39f06a4d0e89abe1c
AlexndreSilva <98479499+AlexndreSilva@users.noreply.github.com>
2022-09-01 16:11:12 -0300

    core module

---
+++ 3_core_modules/index.js
@@ -0,0 +1,7 @@
+//criar a const com o msm nome do modulo
+const patch = require('path')
+
+//verificando qual a extensão de um arquivo com p patch
+const extension = patch.extname('arquivo.py')
+
+console.log(extension)
